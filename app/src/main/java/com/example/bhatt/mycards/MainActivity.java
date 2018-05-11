453d89a6162946011209900876092c78c4415eb5
Bhargav97 <bhattbhargav97@gmail.com>
2018-05-11 08:26:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhatt/mycards/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.bhatt.mycards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    VerticalViewPager verticalViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        verticalViewPager = (VerticalViewPager) findViewById(R.id.verticleViewPager);
+        verticalViewPager.setAdapter(new VerticlePagerAdapter(this));
+    }
+}
